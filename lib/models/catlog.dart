d5bfb50f8b3dba688f9bc9dde994c9b1cc97bcc8
Sakdasariya Parth <parth.social09@gmail.com>
2023-04-29 22:10:57 +0530

    json commit

---
+++ lib/models/catlog.dart
@@ -0,0 +1,17 @@
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final String color;
+  final String imageurl;
+  final num price;
+
+  item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.color,
+      required this.imageurl,
+      required this.price});
+}
+// final products = [item(id: , name: name, desc: desc, color: color, imageurl: imageurl, price: price)]
